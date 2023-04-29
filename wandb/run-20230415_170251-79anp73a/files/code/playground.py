a2db5a59297658d62800f509431bfcde116a8335
Hari Prasad <h4ri.prasad@gmail.com>
2023-04-29 12:10:52 +0530

    init

---
+++ wandb/run-20230415_170251-79anp73a/files/code/playground.py
@@ -0,0 +1,80 @@
+import os
+import argparse
+from distutils.util import strtobool
+from torch.utils.tensorboard import SummaryWriter
+import time
+
+### Arguments ###
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp-name', 
+                        type=str, 
+                        default=os.path.basename(__file__).rstrip('.py'), 
+                        help='Name of Experiment')
+    parser.add_argument('--gym-id',
+                        type=str,
+                        default='CartPole-v1',
+                        help='ID of Gym Environment')
+    parser.add_argument('--learning-rate',
+                        type=float,
+                        default=2.5e-4,
+                        help='Learning rate for the optimizer')
+    parser.add_argument('--seed',
+                        type=int,
+                        default=1,
+                        help='Seed for the experiment')
+    parser.add_argument('--total-timesteps',
+                        type=int,
+                        default=25000,
+                        help='Total number of timesteps')
+    parser.add_argument('--torch-det',
+                        type=lambda x:bool(strtobool(x)),
+                        default=True,
+                        nargs='?',
+                        const=True,
+                        help='If toggled, `torch.backend.cudnn.determentistic=False`')
+    parser.add_argument('--cuda',
+                        type=lambda x:bool(strtobool(x)),
+                        default=True,
+                        nargs='?',
+                        const=True,
+                        help='If toggled, cuda will not be used')
+    parser.add_argument('--track',
+                        type=lambda x:bool(strtobool(x)),
+                        default=False,
+                        nargs='?',
+                        const=True,
+                        help='If toggled, Weignts and biases will be used')
+    parser.add_argument('--wandb-project-name',
+                        type=str,
+                        default='cleanRL',
+                        help='W&B project name')
+    parser.add_argument('--wandb-entity',
+                        type=str,
+                        default=None,
+                        help='The entity of W&B project')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    run_name = f"{args.gym_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    if args.track:
+        import wandb
+
+        wandb.init(
+            project=args.wandb_project_name,
+            entity='qpoch',
+            sync_tensorboard=True,
+            config=vars(args),
+            name=run_name,
+            monitor_gym=True,
+            save_code=True,
+        )
+    writer = SummaryWriter(f"runs/{run_name}")
+    writer.add_text(
+        "hyperparameters",
+        "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+    )
